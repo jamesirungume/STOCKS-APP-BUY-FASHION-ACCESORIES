b4b1123827e097e064e19aaa9a670afdfd60dc96
Your Name <jymohcooler@gmail.com>
2023-07-07 13:40:42 +0300

    added functioning  addtocart buttons

--- index.js
+++ index.js
@@ -1,64 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
-   const shoehat = document.querySelector("#shoehats");
-   const suitwatches = document.querySelector("#suitswatches");
-   const jackettrouser = document.querySelector("#jackettrouser");
- 
-   function fetchItems(category) {
-     return fetch(`http://localhost:3000/${category}`)
-       .then((resp) => resp.json())
-       .then((stormdata) => {
-         console.log(stormdata);
-         displayItems(stormdata, category);
-       })
-       .catch((error) => {
-         console.error("Error fetching data:", error);
-       });
-   }
- 
-   function displayItems(data, category) {
-     const displayContainer = document.querySelector(`#${category}-container`);
-     displayContainer.innerHTML = "";
- 
-     data.forEach((item) => {
-       const { name, price, imgPath } = item;
-       const itemElement = document.createElement("div");
-       itemElement.classList.add("item");
-       itemElement.innerHTML = `
-         <img class="image" src="${imgPath}" alt="${name}">
-         <p class="item-name">${name}</p>
-         <p class="item-price">$${price}</p>
-         <button class = "thebtn" id = "btn" >add to cart</button>
-       `;
-       displayContainer.appendChild(itemElement);
-     });
-   }
- 
-   shoehat.addEventListener("click", () => {
-     fetchItems("shoehats");
-   });
- 
-   suitwatches.addEventListener("click", () => {
-     fetchItems("suitswatches");
-   });
- 
-   jackettrouser.addEventListener("click", () => {
-     fetchItems("jackettrouser");
-   });
+  const shoehat = document.querySelector("#shoehats");
+  const suitwatches = document.querySelector("#suitswatches");
+  const jackettrouser = document.querySelector("#jackettrouser");
+  const createCart = [];
 
-  const mycart = document.querySelector("#cart");
+  function fetchItems(category) {
+    return fetch(`http://localhost:3000/${category}`)
+      .then((resp) => resp.json())
+      .then((stormdata) => {
+        console.log(stormdata);
+        displayItems(stormdata, category);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
 
-  mycart.addEventListener("click", () => {
- 
- 
-   const total = document.createElement("div");
-   total.classList.add = "cartin";
+  function displayItems(data, category) {
+    const displayContainer = document.querySelector(`#${category}-container`);
+    displayContainer.innerHTML = "";
 
-   total.innerHTML = `
-     <h1>Total Price </h1>
-     
-   `;
-   console.log (total)
- });
- 
- });
- 
+    data.forEach((item) => {
+      const { name, price, imgPath } = item;
+      const itemElement = document.createElement("div");
+      itemElement.classList.add("item");
+      itemElement.innerHTML = `
+        <img class="image" src="${imgPath}" alt="${name}">
+        <p class="item-name">${name}</p>
+        <p class="item-price">$${price}</p>
+        <button class="mybtn">add to cart</button>
+      `;
+      const clickbtn = itemElement.querySelector(".mybtn");
+      clickbtn.addEventListener("click", () => {
+       console.log(name);
+       addCart(name, price);
+      });
+
+      displayContainer.appendChild(itemElement);
+    });
+  }
+
+  function addCart(name, price) {
+     const createItem = { name, price };
+     createCart.push(createItem);
+     displayCarts(name, price);
+  }
+
+  function displayCarts(name, price) {
+    const displayCarts = document.createElement("div");
+    displayCarts.classList.add("cart-item");
+    displayCarts.innerHTML = `<p class="name">Name: ${name}</p> <p class="price">Price: ${price}</p>`;
+    document.querySelector("#display1").appendChild(displayCarts);
+  }
+  
+  shoehat.addEventListener("click", () => {
+    fetchItems("shoehats");
+  });
+
+  suitwatches.addEventListener("click", () => {
+    fetchItems("suitswatches");
+  });
+
+  jackettrouser.addEventListener("click", () => {
+    fetchItems("jackettrouser");
+  });
+});
